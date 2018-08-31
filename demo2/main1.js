3843cd2e3dd04d32df6f11597cf49e9a79ad04a4
liuyajun <liuyajun@linhuiba.com>
2018-08-31 11:49:48 +0800

    demo2：多入口文件配置

---
+++ demo2/main1.js
@@ -0,0 +1,2 @@
+document.write('<h1>Hello World</h1>');
+document.write('<h1>This is main1 page</h1>');
---
+++ demo2/main2.js
@@ -0,0 +1,2 @@
+document.write('<h1>Hello Webpack</h1>');
+document.write('<h1>This is main2 page</h1>');
