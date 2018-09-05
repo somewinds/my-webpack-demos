e505f766f9706a59479b2c22575688ba6a8fda58
liuyajun <liuyajun@linhuiba.com>
2018-09-05 15:24:55 +0800

    Demo05: Image loader

---
+++ demo5/main.js
@@ -0,0 +1,11 @@
+let img1 = document.createElement("img");
+img1.src = require("./small.png");
+document.body.appendChild(img1);
+
+let img2 = document.createElement("img");
+img2.src = require("./big.png");
+document.body.appendChild(img2);
+
+let img_gif = document.createElement("img");
+img_gif.src = require("./img_gif.gif");
+document.body.appendChild(img_gif);
