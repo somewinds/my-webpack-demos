af747c0282e5996c8187b60a7c7f4dc4c4571dec
liuyajun <liuyajun@linhuiba.com>
2018-09-10 11:39:15 +0800

    Demo08: HTML Webpack Plugin and Open Browser Webpack Plugin
    1. 窗口名为 Webpack-demos，且index.html文件内容被 main.js 替换
    2. 打开默认8080端口后，OpenBrowserPlugin将打开一个8080端口页面

---
+++ demo8/webpack.config.js
@@ -0,0 +1,45 @@
+var HtmlwebpackPlugin = require('html-webpack-plugin');
+var OpenBrowserPlugin = require('open-browser-webpack-plugin');
+
+module.exports = {
+	// 入口文件，是模块构建的起点，同时每一个入口文件对应最后生成的一个 chunk
+	// chunk：code spliting后的产物，也就是按需加载的分块，装载了不同的module
+	entry: './main.js',
+	// 文件路径指向（可加快打包过程）
+	/*resolve: {
+		alias: {
+			'react': pathToReact
+		}
+	},*/
+	// 生成文件，是模块构建的终点，包括输出文件与输出路径
+	output: {
+		/*path: path.resolve(__dirname, 'build'),*/
+		filename: 'bundle.js'
+	},
+	// 这里配置了处理个模块的 loader，包括 css 预处理 loader，es6 编译 loader，图片处理 loader
+	// loader：能转换各类资源，并处理成对应模块的加载器。loader间可以串行使用
+	/*module: {
+		loaders: [
+			{
+				test: /\.js$/,
+				loader: 'babel',
+				query: {
+					presets: ['es2015', 'react']
+				}
+			}
+		],
+		noParse: [pathToReact]
+	},*/
+	// webpack 各插件对象，在 webpack 的事件流中执行对应的方法
+	plugins: [
+		new HtmlwebpackPlugin({
+			title: 'webpack-demos',
+			filename: 'index.html'
+		}),
+		new OpenBrowserPlugin({
+			url: 'http://localhost:8080'
+		})
+	],
+};
+
+// 在webpack中经常可以看到compilation.plugin('xxx', callback)，可以把它当成是一个事件的绑定，这些事件在打包时由webpack来触发
